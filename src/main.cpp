87556c70cea4607499bc7e2c1919a43302c23714
BanceDev <lanceaborden@protonmail.com>
2024-11-20 14:40:43 -0500

    updated glad

--- src/main.cpp
+++ src/main.cpp
@@ -148,7 +148,7 @@ int main(int argc, char *argv[])
 	// Set OpenGL attributes
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetSwapInterval(1);
 
